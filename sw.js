e10d00ac5ac7e7bb10f31d4477944ea80bd237e6
hanmaners-coder <hanmaners@gmail.com>
2026-09-02 10:06:37 +0900

    V1270: Keep Haeundae Disaster CCTV on Home screen while setting Gwangalli Beach CCTV as primary default in Ocean Spots view

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "aquabuddy-cache-v1260-add-mof-coast-portal-legal-notice";
+const CACHE_NAME = "aquabuddy-cache-v1270-split-home-haeundae-and-spots-gwangalli-cctv";
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
